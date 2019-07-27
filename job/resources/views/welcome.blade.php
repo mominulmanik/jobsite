88a05588fe7b01255c4fdf7b410830f02a319491
mominulmanik <44928032+mominulmanik@users.noreply.github.com>
2019-07-28 01:03:05 +0600

    Update welcome.blade.php

--- job/resources/views/welcome.blade.php
+++ job/resources/views/welcome.blade.php
@@ -81,17 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Job Site
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <img src="jobs.jpg" width="100%" height="300px;">
                 </div>
             </div>
         </div>
